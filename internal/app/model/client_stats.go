df59bcc4925a2ae5fa6b3594020621f893536091
Jiang <jiangzhiguo1992@163.com>
2025-01-13 21:29:55 +0800

    upd model client_stats

--- internal/app/model/client_stats.go
+++ internal/app/model/client_stats.go
@@ -1,14 +1,37 @@
 package model
 
 const (
-	ClientStatsKindWatched  = 1 // 查看Watched (web端的查看，和应用市场的详情页进入)
-	ClientStatsKindDownload = 2 // 下载Download (小程序的话，关注也是这个)
-	ClientStatsKindOpener   = 3 // 启动Opener
-	ClientStatsKindRegister = 4 // 注册Register (转化etc)
-	ClientStatsKindActive   = 5 // 激活Active (日/周/月/年活, 留存/回归率)
-	ClientStatsKindReturn   = 5 // 回归Active (日/周/月/年活, 留存/回归率)
-	ClientStatsKindPay      = 6 // 付费数Payer (日/周/月/年付费, 会员率/续费率/转化率/付费率/平均每付费用户收入/每用户平均收入)
-	ClientStatsKindScore    = 7 // 评分Score
+	ClientStatsKindWatched  = 1 // 查看 (web端的查看，和应用市场的详情页进入)
+	ClientStatsKindDownload = 2 // 下载 (小程序的话，关注也是这个)
+	ClientStatsKindStart    = 3 // 首次启动
+	ClientStatsKindOpener   = 4 // 启动(包括首次)
+	ClientStatsKindOpenerU  = 5 // 启动(包括首次+去重)
+	ClientStatsKindRegister = 6 // 首次注册
+	ClientStatsKindActive   = 7 // 登录(包括首次)
+	ClientStatsKindActiveU  = 8 // 登录(包括首次+去重)
+	ClientStatsKindScore    = 9 // 评分 (评论放链接里点击)
+
+	ClientStatsKindDuration1   = 10 // 在线时长 (min)
+	ClientStatsKindDuration5   = 11 // 在线时长 (min)
+	ClientStatsKindDuration10  = 12 // 在线时长 (min)
+	ClientStatsKindDuration30  = 13 // 在线时长 (min)
+	ClientStatsKindDuration60  = 14 // 在线时长 (min)
+	ClientStatsKindDuration180 = 15 // 在线时长 (min)
+	ClientStatsKindReturn7     = 16 // 回归周期 (day)
+	ClientStatsKindReturn15    = 17 // 回归周期 (day)
+	ClientStatsKindReturn30    = 18 // 回归周期 (day)
+	ClientStatsKindReturn90    = 19 // 回归周期 (day)
+
+	ClientStatsKindPayer       = 20 // 付费用户数(去重)
+	ClientStatsKindPayFirst    = 21 // 首充次数
+	ClientStatsKindPayCount    = 22 // 付费次数(包括首充)
+	ClientStatsKindPayMoney1   = 23 // 付费金额 (元)
+	ClientStatsKindPayMoney6   = 24 // 付费金额 (元)
+	ClientStatsKindPayMoney30  = 25 // 付费金额 (元)
+	ClientStatsKindPayMoney68  = 26 // 付费金额 (元)
+	ClientStatsKindPayMoney128 = 27 // 付费金额 (元)
+	ClientStatsKindPayMoney328 = 28 // 付费金额 (元)
+	ClientStatsKindPayMoney648 = 29 // 付费金额 (元)
 )
 
 // ClientStats 客户端统计量
